194be32c1216f7cc4be1f235817ae6e03957b6ce
Ansible automation codes <144895375+ansible-codes@users.noreply.github.com>
2023-12-14 14:45:45 +0000

    Update RepointDB_response.sh

--- RepointDB_response.sh
+++ RepointDB_response.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Function to check if a variable is empty
+check_empty() {
+    local var_name="$1"
+    local var_value="${!var_name}"
+
+    if [ -z "$var_value" ]; then
+        echo "Error: Parameter '$var_name' is empty."
+        exit 1
+    fi
+}
+
 # Get the current path
 current_path=$(pwd)
 
@@ -21,24 +32,23 @@ do
     # Remove spaces from key
     key=$(echo "$key" | tr -d ' ')
 
-    # Check if the value is empty
-    if [ -z "$value" ]; then
-        echo "Error: Parameter '$key' is empty."
-        exit 1
-    fi
-
     # Assign the value to the variable named as key
     eval "$key=\"$value\""
 done < "$file_path"
 
-# Example: Print all variables
+# Check each variable for emptiness
+check_empty "Griffin_Jira_number"
+check_empty "Point_to_UI_host"
+check_empty "GriffinDB_host"
+check_empty "GriffinDB_name"
+check_empty "GriffinDB_credentials"
+check_empty "GriffinAnalyticsDB_host"
+check_empty "GriffinAnalyticsDB_name"
+check_empty "GriffinAnalyticsDB_credentials"
+
+# Example: Print all variables if none are empty
 echo "Current Path: $current_path"
 echo "Griffin_Jira_number: $Griffin_Jira_number"
 echo "Point_to_UI_host: $Point_to_UI_host"
 echo "GriffinDB_host: $GriffinDB_host"
-echo "GriffinDB_name: $GriffinDB_name"
-echo "GriffinDB_credentials: $GriffinDB_credentials"
-echo "GriffinAnalyticsDB_host: $GriffinAnalyticsDB_host"
-echo "GriffinAnalyticsDB_name: $GriffinAnalyticsDB_name"
-echo "GriffinAnalyticsDB_credentials: $GriffinAnalyticsDB_credentials"
 # ... (continue with other variables as required)
