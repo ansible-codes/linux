0a3fa19d5420ae35af602e4c7911ee7918b89f5c
Ansible automation codes <144895375+ansible-codes@users.noreply.github.com>
2023-12-14 11:19:43 +0000

    Create RepointDB_response.sh

---
+++ RepointDB_response.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Read the file line by line
+while IFS= read -r line
+do
+    # Extract the key and value from each line
+    key=$(echo "$line" | cut -d ':' -f 1)
+    value=$(echo "$line" | cut -d ':' -f 2)
+
+    # Remove spaces from key
+    key=$(echo "$key" | tr -d ' ')
+
+    # Assign the value to the variable named as key
+    eval "$key=\"$value\""
+done < /path/to/your/file/RepointDB_responseFile.txt
+
+# Example: Print the variables
+echo "Griffin_Jira_number: $Griffin_Jira_number"
+echo "Point_to_UI_host: $Point_to_UI_host"
+echo "GriffinDB_host: $GriffinDB_host"
+echo "GriffinDB_name: $GriffinDB_name"
+echo "GriffinDB_credentials: $GriffinDB_credentials"
+echo "GriffinAnalyticsDB_host: $GriffinAnalyticsDB_host"
+echo "GriffinAnalyticsDB_name: $GriffinAnalyticsDB_name"
+echo "GriffinAnalyticsDB_credentials: $GriffinAnalyticsDB_credentials"
